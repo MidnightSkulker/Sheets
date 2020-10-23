c36188182cebfb5e9bb02800a61d5afb0e6c4152
Peter White <desourdesourde@gmail.com>
2020-10-22 19:36:09 -0700

    Removed credentials

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import openSheets
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -11,6 +12,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+
 
 def main():
     """Shows basic usage of the Sheets API.
