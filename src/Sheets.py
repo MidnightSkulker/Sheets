5ac652a8739edb12e3f02170dca215275496a9f6
Peter White <desourdesourde@gmail.com>
2020-11-04 11:07:51 -0800

    Added comments to Sheets.py

--- src/Sheets.py
+++ src/Sheets.py
@@ -3,21 +3,21 @@ from sheetfu import *
 import re
 from webcolors import *
 
-red = '#ff0000'
-blue = '#0000ff'
-green = '#37761c'
-purple = '#9900ff'
-black = '#000000'
-colorsToIgnore = [red, blue, green, purple]
-namesToIgnore = ['Totals', 'Remaining', 'Check Sum', 'Paid', 'Debt']
-monthRow = 2
+red = '#ff0000'    # Ignore this cell
+blue = '#0000ff'   # Paid
+green = '#37761c'  # Paid Cash
+purple = '#9900ff' # Paid off with debt from March
+black = '#000000'  # Due
+colorsToIgnore = [red, blue, green, purple] # Colors ignore in the student names column.
+namesToIgnore = ['Totals', 'Remaining', 'Check Sum', 'Paid', 'Debt'] # Names to ignore in the student names Column.
+monthRow = 2       # Row containing the months (Sep, Oct, ...)
 
-# Determine the payment status
+# Determine the payment status, based on the color of the entry.
 def paymentStatus(amount: str, color: str) -> str:
     status = 'Unknown'
     if amount == None or amount == '':
         status = 'No Charge'
-    elif color == black: # Black
+    elif color == black:
         status = 'Due'
     elif color == blue:
         status = 'Paid'
@@ -27,7 +27,7 @@ def paymentStatus(amount: str, color: str) -> str:
         status = 'Cash'
     return status
 
-# Find a column for the specified month
+# Find a column for the specified month in the row that has month headings.
 def getMonthCol(data_range: object, month: str) -> int:
     values = data_range.get_values()
     maxCol = data_range.get_max_column()
@@ -53,24 +53,33 @@ def loadSheet (sheetId: str, sheetName: str) -> object:
         return None
 
 def getStudentSheetInfo(data_range: object, month: int) -> list:
-    values = data_range.get_values()
-    colors = data_range.get_font_colors()
-    maxRow = data_range.get_max_row()
+    values = data_range.get_values()        # Cell values.
+    colors = data_range.get_font_colors()   # Cell font colors.
+    maxRow = data_range.get_max_row()       # Number of rows used in the sheet.
     # Determine which column is for the month requested.
     monthCol = getMonthCol(data_range, month)
-    if monthCol == None: return None
+    if monthCol == None:
+        print('-*-*-*-*-> No column found for month:', month)
+        return None
     names = []
     for j in range(0,maxRow):
-        potentialStudent = values[j][0] # Student Name
-        attendanceStatus = values[j][1] # Gone or Break
-        charge = values[j][monthCol]
-        chargeColor = colors[j][monthCol]
+        potentialStudent = values[j][0]   # Student Name.
+        attendanceStatus = values[j][1]   # Gone or Break.
+        charge = values[j][monthCol]      # Charge for the student for the month.
+        chargeColor = colors[j][monthCol] # Font color of the charge.
+        # Ignore student names that are numeric.
         if str(potentialStudent).isnumeric(): continue
+        # Ignore students names that are an empty cell.
         if potentialStudent is None or potentialStudent == '': continue
+        # Ignore specified names.
         if potentialStudent in namesToIgnore: continue
+        # Get the color of the student name.
         color = colors[j][0]
+        # Ignore the specified colors.
         if color in colorsToIgnore: continue
+        # Ignore students on break or gone.
         if (attendanceStatus == 'Break') or (attendanceStatus == 'Gone'): continue
+        # Compute the charge color from the rgb hex value for the cell with the charge.
         if not chargeColor: chargeColor = '#000000' # black
         chargeStatus = paymentStatus(charge, chargeColor)
         try:
@@ -81,12 +90,12 @@ def getStudentSheetInfo(data_range: object, month: int) -> list:
             else:
                 print('ERROR: Charge Color ', chargeColor)
                 chargeColor = 'purple'
-
+        # Smash the relevant information together into a dict.
         studentInfo = { 'name': potentialStudent
                       , 'charge': charge
                       , 'color': chargeColor
                       , 'status': chargeStatus }
-        names.append(studentInfo)
+        names.append(studentInfo) # Add to the list of students.
     return names
         
 # Read the first column, and get all the student names and their row numbers.
