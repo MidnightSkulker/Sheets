cd3311e8fc7b0de5af7e31e3102aa5a0f6b16a9f
Peter White <desourdesourde@gmail.com>
2020-11-04 10:59:20 -0800

    Removed unused sheet id

--- src/SheetIds.py
+++ src/SheetIds.py
@@ -1,4 +1,3 @@
 # Ids of all the spreadsheets needed
 graceSheet = '1fLDy8hmgYdxYvQPhbi6DFL0BqR8hvlvK2YaA7vgoEzc'
 mythiliSheet = '1MolY8_4Ze-g8vxVhd9MLg0yCq8asqY-JAk4q673zlf8'
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
