43d500927dbd5317d3d5be6a33431d2d6548b83e
Peter White <desourdesourde@gmail.com>
2020-11-04 10:58:41 -0800

    Formatting in Months.py

--- src/Months.py
+++ src/Months.py
@@ -3,7 +3,7 @@ import re
 months3Letters = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 # Determine if a string matches a month
-def isMonth(s:str):
+def isMonth(s: str):
     for m in months3Letters:
         if re.match(m, s): return m
     return None
